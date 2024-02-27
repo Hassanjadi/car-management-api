cb79e3d5bdaa4bf15d5e7a747b63cbdc1ca7d7f6
Hassanjadi <hassanjadi25@gmail.com>
2024-02-27 17:07:10 +0700

    refactor

---
+++ src/middleware/auth.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express'
+
+export const requireUser = (req: Request, res: Response, next: NextFunction) => {
+  const user = res.locals.user
+  if (!user) {
+    return res.status(403).send('Forbidden: User is required')
+  }
+
+  return next()
+}
+
+export const requireAdmin = (req: Request, res: Response, next: NextFunction) => {
+  const user = res.locals.user
+  if (!user || user.role === 'Member') {
+    return res.status(403).send('Forbidden: Admin role is required')
+  }
+
+  return next()
+}
+
+export const requireSuperadmin = (req: Request, res: Response, next: NextFunction) => {
+  const user = res.locals.user
+  if (!user || user.role !== 'Superadmin') {
+    return res.status(403).send('Forbidden: Superadmin role is required')
+  }
+
+  return next()
+}

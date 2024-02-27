cb91a747c7b561df689dedcee1ad1f54760b6faf
Hassanjadi <hassanjadi25@gmail.com>
2024-02-27 17:04:00 +0700

    feat: add auth validation

---
+++ src/validations/authValidation.ts
@@ -0,0 +1,31 @@
+import Joi from 'joi'
+import UserType from '../types/userType'
+
+export const createUserValidation = (payload: UserType) => {
+  const schema = Joi.object({
+    id: Joi.string().required(),
+    username: Joi.string().required(),
+    email: Joi.string().required(),
+    password: Joi.string().required(),
+    role: Joi.string()
+  })
+
+  return schema.validate(payload)
+}
+
+export const createSessionValidation = (payload: UserType) => {
+  const schema = Joi.object({
+    email: Joi.string().required(),
+    password: Joi.string().required()
+  })
+
+  return schema.validate(payload)
+}
+
+export const refreshSessionValidation = (payload: UserType) => {
+  const schema = Joi.object({
+    refreshToken: Joi.string().required()
+  })
+
+  return schema.validate(payload)
+}

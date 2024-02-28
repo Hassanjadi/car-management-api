e51c31ab09aa21c29129b945b2bf20241271a418
Hassanjadi <hassanjadi25@gmail.com>
2024-02-28 18:20:41 +0700

    feat: require user

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -1,5 +1,5 @@
 import { gettingCars, createCars, updateCars, deleteCars } from '../controllers/carController'
-import { requireAdmin } from '../middleware/auth'
+import { requireAdmin, requireUser } from '../middleware/auth'
 import upload from '../utils/upload'
 import { Router } from 'express'
 
@@ -7,7 +7,7 @@ export const CarRouter: Router = Router()
 
 // router car
 CarRouter.get('/v1/cars', gettingCars)
-CarRouter.get('/v1/cars/:id', gettingCars)
+CarRouter.get('/v1/cars/:id', requireUser, gettingCars)
 CarRouter.post('/v1/cars', requireAdmin, upload.single('image'), createCars)
 CarRouter.put('/v1/cars/:id', requireAdmin, upload.single('image'), updateCars)
 CarRouter.delete('/v1/cars/:id', requireAdmin, deleteCars)
